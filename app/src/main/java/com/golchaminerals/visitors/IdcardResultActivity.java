a6afe070c62403152f68675e58ffff5aae3ce2f8
sharmajiii <vipul.vs12@gmail.com>
2020-07-09 19:28:39 +0530

    ui design for qrcode

---
+++ app/src/main/java/com/golchaminerals/visitors/IdcardResultActivity.java
@@ -0,0 +1,13 @@
+package com.golchaminerals.visitors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IdcardResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idcard_result);
+    }
+}
